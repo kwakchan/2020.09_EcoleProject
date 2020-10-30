51c9cea3c6d02bd37445cbf32965deae65c6cbfb
JongJin, Kim <whdwls3486@gmail.com>
2020-10-31 00:11:37 +0900

    경기 생성 페이지 추가

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, {Component} from 'react';
+import { View, Text, Button, TextInput, StyleSheet} from 'react-native';
+
+
 
 const MainScreen = ({ navigation }) => {
 
+
   return (
     <View style={{ flex: 1, padding: 30 }}>
-      <View style={{ flex: 1 }}>
-        <Text style={{ fontSize: 30 }}>Member List</Text>
+      <View style={{ flex: 2 }}>
+        <Text style={{ fontSize: 30 , textAlign: "center"}}>경기 개설</Text>
+      </View>
+      <View>
+      <Text style={{ fontSize: 30 }}>시간</Text>
+      </View>
+      <View>
+      <Text style={{ fontSize: 30 }}>지역</Text>
+      </View>
+      <View>
+      <Text style={{ fontSize: 30 }}>인원 수</Text>
+      </View>
+      <View>
+      <Text style={{ fontSize: 30 }}>세부사항</Text>
+      <TextInput style = {styles.input}
+        underlineColorAndroid = "transparent"
+        placeholder = "내용을 입력해주세요"
+        placeholderTextColor = "#9a73ef"
+        autoCapitalize = "none"
+      />
       </View>
       <View style={{ flex: 1 }}>
         <Button
-          title="Add Member"
+          title="Match Create"
           onPress={() => navigation.navigate('AddMember') }
         />
       </View>
@@ -21,3 +42,13 @@ const MainScreen = ({ navigation }) => {
 }
 
 export default MainScreen;
+
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    height: 120,
+    borderColor: '#7a42f4',
+    borderWidth: 1
+  }
+  
+})
