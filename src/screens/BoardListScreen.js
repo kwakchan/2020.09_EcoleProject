85f1dd8d735c343fea72435415be1d8c4e6644ef
Chan, Kwak <rhkrcks3@gmail.com>
2020-11-27 20:51:02 +0900

    teamList 완성

--- src/screens/BoardListScreen.js
+++ src/screens/BoardListScreen.js
@@ -14,7 +14,7 @@ async function getBoardList(setBoards){
         Authorization: token
       }
     }
-    const res = await api.get('/api/boards', config);
+    const res = await api.get(`/api/boards?search=${search}`, config);
     setBoards(res.data);
   } catch (error) {
     console.log(error)
@@ -27,7 +27,7 @@ const BoardListScreen = ({ navigation }) => {
   const [boards, setBoards] = useState([]);
 
   useEffect(() => {
-    getBoardList(setBoards);
+    getBoardList(setBoards, search);
   }, [])
 
   return (
@@ -67,7 +67,7 @@ const BoardListScreen = ({ navigation }) => {
         />
 
         <Button
-          title="팀 만들기"
+          title="게시글 만들기"
           onPress={() => navigation.navigate('BoardCreate')}
         />
 
